a93189351a9b4f665983d4c07e1bde85f6dee8d6
Alicia Olivares Canales <alicia93oc@gmail.com>
2025-02-09 19:37:01 +0100

    mongodb connection completed

---
+++ server/config/mongodbConnection.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+process.env.DATABASE_MONGODB_URI;
+
+const dbname = "Monthly-expenses";
+const uri = process.env.DATABASE_MONGODB_URI;
+
+async function run() {
+  try {
+    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+    await mongoose.connect(uri);
+   
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await mongoose.disconnect();
+  }
+}
+
+module.exports = run;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,31 @@
 require("dotenv").config();
 const express = require("express");
-const mongoose = require("mongoose");
-
 const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+const PORT = process.env.PORT || 3500
+const cors = require("cors");
+const connectDB = require("./config/mongodbConnection");
+const { log } = require("console");
+
+connectDB();
+app.use(cors());
+app.set("view engine", "ejs");
+app.use(express.json());
+
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ message: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
+});
+
+mongoose.connection.on("error", (err) => {
+  log(err);
+});
+
+app.listen(PORT, () => log("Server started on port 3500"));
