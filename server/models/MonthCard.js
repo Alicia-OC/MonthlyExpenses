49ad608137617b922177a6d5abe3d5450d38d255
Alicia Olivares Canales <alicia93oc@gmail.com>
2025-08-04 23:09:37 +0200

    fixed bugs in models

--- server/models/MonthCard.js
+++ server/models/MonthCard.js
@@ -14,73 +14,94 @@ const monthCardSchema = new mongoose.Schema(
 
     totalSavings: { type: Number, required: true, default: 0 },
     fixedItems: {
-      name: 'The Non-negotiables',
-      items: [{
-        description: String,
-        price: Number,
-        date: Date,
-      }]
+      name: {
+        type: String,
+        default: "The Non-negotiables",
+      },
+      items: [
+        {
+          description: { type: String, required: true },
+          price: { type: Number, required: true },
+          date: { type: Date, default: Date.now },
+        },
+      ],
     },
     fixedExpenses: { type: Number, required: true, default: 0 },
 
     subscriptionItems: {
-      name: 'On Repeat',
-      items: [{
-        description: String,
-        price: Number,
-        date: Date,
-      }]
-    },
-    subscriptionExpenses: { type: Number, required: true, default: 0 },
-    
-    otherItems: {
-      name: 'Little Life Things',
+      name: {
+        type: String,
+        default: "On Repeat",
+      },
       items: [
         {
-          description: String,
-          price: Number,
+          description: { type: String, required: true },
+          price: { type: Number, required: true },
+          date: { type: Date, default: Date.now },
+        },
+      ],
+    },
+    subscriptionExpenses: { type: Number, required: true, default: 0 },
+
+    otherItems: {
+      name: {
+        type: String,
+        default: "Little Life Things",
+      },
+      items: [
+        {
+          description: { type: String, required: true },
+          price: { type: Number, required: true },
           category: {
             type: mongoose.Schema.Types.ObjectId,
-            ref: "Category", // Reference to the Category model
+            ref: "Category", //reference to the Category model
             required: true,
           },
-          date: Date,
+          date: { type: Date, default: Date.now },
         },
-      ]
+      ],
     },
+
     otherExpenses: { type: Number, required: true, default: 0 },
 
     transportItems: {
-      name: 'Out & About',
+      name: {
+        type: String,
+        default: "Out & About",
+      },
       items: [
         {
-          description: String,
-          price: Number,
+          description: { type: String, required: true },
+          price: { type: Number, required: true },
           category: {
             type: mongoose.Schema.Types.ObjectId,
-            ref: "Category", // Reference to the Category model
+            ref: "Category",
             required: true,
           },
-          date: Date,
+          date: { type: Date, default: Date.now },
         },
-      ]
+      ],
     },
+
     transportExpenses: { type: Number, required: true, default: 0 },
 
     foodItems: {
-      name: 'Bits & Bites',
+      name: {
+        type: String,
+        default: "Bits & Bites",
+      },
       items: [
         {
-          description: String,
-          price: Number,
+          description: { type: String, required: true },
+          price: { type: Number, required: true },
           category: {
             type: mongoose.Schema.Types.ObjectId,
-            ref: "Category", // Reference to the Category model
+            ref: "Category",
             required: true,
           },
-          date: Date,
+          date: { type: Date, default: Date.now },
         },
-      ]
+      ],
     },
     foodExpenses: { type: Number, required: true, default: 0 },
   },
@@ -88,71 +109,5 @@ const monthCardSchema = new mongoose.Schema(
 );
 
 const MonthCard = mongoose.model("MonthCard", monthCardSchema);
-
-const mockupMonthCard = new MonthCard({
-  user: "67a910c50f2e7168975baaf6",
-  year: 2024,
-  month: 2,
-  totalExpenses: 1200,
-  totalIncome: 1600,
-  totalSavings: 400,
-  fixedItems: [
-    {
-      description: "rent+bills+food",
-      price: 605,
-      date: new Date("2025-02-01"),
-    },
-  ],
-  subscriptionItems: [
-    {
-      description: "HBO",
-      price: 4.99,
-      date: new Date("2025-02-01"),
-    },
-    {
-      description: "Amazon no ads",
-      price: 1.99,
-      date: new Date("2025-02-01"),
-    },
-  ],
-  otherItems: [
-    {
-      description: "print label + adhesive",
-      price: 2.56,
-      category: "67a91f012213777227c723ca",
-      date: new Date("2025-02-01"),
-    },
-    {
-      description: "Decathlon",
-      price: 76,
-      category: "67a91f012213777227c723cb",
-      date: new Date("2025-02-01"),
-    },
-  ],
-  transportItems: [
-    {
-      description: "cabify",
-      price: 6.98,
-      category: "67a91f012213777227c723cb",
-
-      date: new Date("2025-02-01"),
-    },
-  ],
-  foodItems: [
-      {
-        description: 'cinnamon oreo',
-        price: 4.99,
-        category: '67a91f012213777227c723cb',
-
-        date: new Date(),
-      },
-    ],
-
-  fixedExpenses: 9,
-  subscriptionExpenses: 9,
-  otherExpenses: 9,
-  transportExpenses: 9,
-  foodExpenses: 24
-});
 
 module.exports = { MonthCard };
